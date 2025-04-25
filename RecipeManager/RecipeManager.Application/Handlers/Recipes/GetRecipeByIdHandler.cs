a73d1f15b963ecf820d79d28f5c9ed10810fc0fe
Guillermo López Pérez <glopez@visiotechsecurity.com>
2025-04-25 11:17:30 +0200

    Implement get recipe by id

---
+++ RecipeManager/RecipeManager.Application/Handlers/Recipes/GetRecipeByIdHandler.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using FluentResults;
+using MediatR;
+using RecipeManager.Application.DTO.Recipes;
+using RecipeManager.Application.Queries.Recipes;
+using RecipeManager.Domain.Interfaces.Repositories;
+
+namespace RecipeManager.Application.Handlers.Recipes
+{
+    public class GetRecipeByIdHandler : IRequestHandler<GetRecipeByIdQuery, Result<RecipeDto>>
+    {
+        private readonly IRecipeRepository _recipeRepository;
+        private readonly IMapper _mapper;
+
+        public GetRecipeByIdHandler(IRecipeRepository recipeRepository, IMapper mapper)
+        {
+            _recipeRepository = recipeRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Result<RecipeDto>> Handle(GetRecipeByIdQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var recipe = await _recipeRepository.GetByIdAsync(request.Id, cancellationToken);
+
+                var recipeDto = _mapper.Map<RecipeDto>(recipe);
+
+                return Result.Ok(recipeDto);
+            }
+            catch (KeyNotFoundException knf)
+            {
+                return Result.Fail<RecipeDto>(knf.Message);
+            }
+            catch (Exception ex)
+            {
+                return Result.Fail<RecipeDto>("Error while obtaining recipe by Id").WithError(ex.Message);
+            }
+        }
+    }
+}
---
+++ RecipeManager/RecipeManager.Application/Queries/Recipes/GetRecipeByIdQuery.cs
@@ -0,0 +1,8 @@
+﻿using FluentResults;
+using MediatR;
+using RecipeManager.Application.DTO.Recipes;
+
+namespace RecipeManager.Application.Queries.Recipes
+{
+    public record GetRecipeByIdQuery(Guid Id): IRequest<Result<RecipeDto>>;
+}
