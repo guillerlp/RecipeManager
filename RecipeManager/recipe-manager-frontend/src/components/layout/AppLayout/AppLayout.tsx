e239f0fe8ff7abff41064a0d2b04fc0132cabb15
guillerlp99 <guillermo.lopezp@edu.uah.es>
2025-08-13 20:42:47 +0100

    Implement AppLayout style

--- RecipeManager/recipe-manager-frontend/src/components/layout/AppLayout/AppLayout.tsx
+++ RecipeManager/recipe-manager-frontend/src/components/layout/AppLayout/AppLayout.tsx
@@ -1,20 +1,21 @@
-import { Divider } from "@mui/material";
-import styles from "./AppLayout.module.css"
-import { Header } from "@/components";
+import { Box } from "@mui/material";
+import styles from "./AppLayout.module.css";
+import { Footer, Header } from "@/components";
 
 interface AppLayoutProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 export const AppLayout = ({ children }: AppLayoutProps) => {
-    return (
-        <div className={styles.appWrapper}>
-            <Header />
-            <Divider />
-            <main>
-                {children}
-            </main>
-            <Divider />
-        </div>
-    )
-} 
+  return (
+    <div className={styles.appShell}>
+      <Header />
+      <Box component="main" className={styles.main} role="main">
+        {children}
+      </Box>
+      <Footer />
+    </div>
+  );
+};
+
+export default AppLayout;
