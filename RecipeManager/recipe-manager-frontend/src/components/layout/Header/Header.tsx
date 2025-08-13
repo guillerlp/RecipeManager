2f84e6470c673e78a45509b8d95190d0c26ad11e
guillerlp99 <guillermo.lopezp@edu.uah.es>
2025-08-13 20:41:12 +0100

    Implement header style

--- RecipeManager/recipe-manager-frontend/src/components/layout/Header/Header.tsx
+++ RecipeManager/recipe-manager-frontend/src/components/layout/Header/Header.tsx
@@ -1,19 +1,36 @@
 // components/layout/header/Header.tsx
-
 import { Logo } from '@/components/ui';
 import styles from './Header.module.css';
 import { NavLink } from '@/components/ui/NavLink/NavLink.js';
 
-export const Header:React.FC = () => {  
-    return (
-        <div>
-            <div className={styles.logoDiv}>
-                <Logo/>
-                <div className={styles.navLinkDiv}>
-                    <NavLink to='/'>Home</NavLink>
-                    <NavLink to='/profile'>Profile</NavLink>
-                </div>
-            </div>
-        </div>
-    )
-}
+export const Header: React.FC = () => {
+  return (
+    <header className={styles.headerBar}>
+      <div className={styles.brand}>
+        <Logo />
+      </div>
+
+      <nav aria-label="Primary navigation" className={styles.nav}>
+        <ul className={styles.navList}>
+          <li>
+            <NavLink to="/">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/recipes">
+              Recipes
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/profile">
+              Profile
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
